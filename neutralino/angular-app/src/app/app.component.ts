82829903cc46d876eba195a0a1cf8f319b826222
Rusty <rgbrewer@gmail.com>
2022-09-24 17:47:17 -0400

    quality of life

--- neutralino/angular-app/src/app/app.component.ts
+++ neutralino/angular-app/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-app';
+
+
+  ngOnInit() {
+    // @ts-ignore
+    // window.myApp.setTray();
+  }
 }
